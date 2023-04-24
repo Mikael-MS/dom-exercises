bfe4ca2b877e03664e3cbf0e04d508504a2e627e
Mikael Moura <mikael.moura79@gmail.com>
2023-04-24 17:56:10 -0300

    fix the code

--- javascript/app.js
+++ javascript/app.js
@@ -1,93 +1,98 @@
 
 function addUser() {
+    // Essa parte faz referencia aos inputs
+    let firstNameEl = document.getElementById('first-name');
+    let lastNameEl = document.getElementById('last-name');
+    let yearOfBirthEl = document.getElementById('year-of-birth');
+
     // Essa parte recupera os valores dos inputs
-    let firstName = document.getElementById('first-name').value; 
-    let lastName = document.getElementById('last-name').value;
-    let yearOfBirth = document.getElementById('year-of-birth').value;
+    let firstName = firstNameEl.value; 
+    let lastName = lastNameEl.value;
+    let yearOfBirth = Number(yearOfBirthEl.value);
 
     //Essa parte da o ano atual e calcula idade do usuario
     const data = new Date();
     const year = data.getFullYear();
     const age = year - yearOfBirth;
 
-    // Essa const é o ano maximo que um usuario pode ter nascido
-    const maxYearOfBirth = 1873;
+    const errorFirstName = document.getElementById('error-firstname');
+    const errorLastName = document.getElementById('error-lastname');
+    const errorYearOfBirth = document.getElementById('error-yearofbirth');
 
+     
     //Essa parte valida o input firstName
     if (!firstName) {
-        document.getElementById('error-one').innerHTML = 'Required field';
+        errorFirstName.innerHTML = 'Required field';
     } else if (firstName.length < 3) {
-        document.getElementById('error-one').innerHTML = 'More than 3 letters';
+        errorFirstName.innerHTML = 'First name must be at least 3 letters';
     } else if (firstName.length > 25) {
-        document.getElementById('error-one').innerHTML = 'Less than 25 letters';
+        errorFirstName.innerHTML = 'The first name must have a maximum of 25 letters';
     } else if (!isNaN(firstName)) {
-        document.getElementById('error-one').innerHTML = 'Only letters';
+        errorFirstName.innerHTML = 'This field only accepts letters';
         return
     } else {
-        document.getElementById('error-one').innerHTML = '';
+        errorFirstName.innerHTML = '';
     }
 
     //Essa parte valida o input lastName
     if (!lastName) {
-        document.getElementById('error-two').innerHTML = 'Required field';
+        errorLastName.innerHTML = 'Required field';
     } else if (lastName.length < 3) {
-        document.getElementById('error-two').innerHTML = 'More than 3 letters';
+        errorLastName .innerHTML = 'Last name must be at least 3 letters';
     } else if (lastName.length > 25) {
-        document.getElementById('error-two').innerHTML = 'Less than 25 letters';
+        errorLastName .innerHTML = 'The last name must have a maximum of 25 letters';
     } else if (!isNaN(lastName)) {
-        document.getElementById('error-two').innerHTML = 'Only letters';
+        errorLastName .innerHTML = 'This field only accepts letters';
     } else {
-        document.getElementById('error-two').innerHTML = '';
+        errorLastName .innerHTML = '';
     }
 
     //Essa parte valida o input yearOfBirth
     if (!yearOfBirth) {
-        document.getElementById('error-tree').innerHTML = 'Required field';
+        errorYearOfBirth.innerHTML = 'Required field';
     } else if (isNaN(yearOfBirth)) {
-        document.getElementById('error-tree').innerHTML = 'Only numbers';
-    } else if (yearOfBirth.length < 4) {
-        document.getElementById('error-tree').innerHTML = 'Minimum 4 numbers';
+        errorYearOfBirth.innerHTML = 'This field only accepts numbers';
+    } else if (yearOfBirth.toString().length < 4 ) {
+        errorYearOfBirth.innerHTML = 'year of birth must have at least 4 numbers';
     } else if (yearOfBirth >= year) {
-        document.getElementById('error-tree').innerHTML = 'Must be less than: ' + year;
-    } else if (yearOfBirth <= maxYearOfBirth) {
-        document.getElementById('error-tree').innerHTML = 'Maximum birth year: ' + maxYearOfBirthsim ;
+        errorYearOfBirth.innerHTML = 'year of birth must be less than: ' + year;
+    } else if (yearOfBirth < (year - 150)) {
+        errorYearOfBirth.innerHTML = 'Maximum age: ' + 150;
     } else {
-        document.getElementById('error-tree').innerHTML = '';
+        errorYearOfBirth.innerHTML = '';
     }
    
     //Essa parte verifiva se tem algum erro dispardo e para o codigo
-    if(document.getElementById('error-one').innerHTML.length > 0 ||
-        document.getElementById('error-two').innerHTML.length > 0||
-        document.getElementById('error-tree').innerHTML.length > 0) {
-        console.log('para codigo')
+    if(errorFirstName.innerHTML.length > 0 ||
+        errorLastName.innerHTML.length > 0||
+        errorYearOfBirth.innerHTML.length > 0) {
         return
     }
-
+   
     //Essa parte insere uma linha  na tabela cada vez que entra um usuario para lista
     let table = document.getElementById('users-table');
-    const tr = document.createElement("tr")
-    table.appendChild(tr)
+    const tr = document.createElement("tr");
+    table.appendChild(tr);
 
     // Evento de click que remove os dados ao clicar na linha da tabela
     tr.addEventListener('click', function (el) {
-        el.target.parentNode.remove()
+        el.target.parentNode.remove();
     })
 
     //Insere as colunas e ecreve os dados
     const name = document.createElement("td")
     name.innerHTML = firstName + ' ' + lastName;
-    tr.appendChild(name)
+    tr.appendChild(name);
 
     const ageEl = document.createElement("td")
-    ageEl.innerHTML = age
-    tr.appendChild(ageEl)
+    ageEl.innerHTML = age;
+    tr.appendChild(ageEl);
 
-  
 
     //Essa parte esvazia os inputs no final 
-    document.getElementById('first-name').value = '';
-    document.getElementById('last-name').value = '';
-    document.getElementById('year-of-birth').value = '';
+    firstNameEl.value = '';
+    lastNameEl.value = '';
+    yearOfBirthEl.value = '';
 }
 
 
